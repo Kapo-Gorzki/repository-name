dd668a3a2cdce52054a722ebf7de26dde75fe01a
Kapo-Gorzki <79355604+Kapo-Gorzki@users.noreply.github.com>
2021-02-27 10:22:32 +0100

    Gracz/Kamera

---
+++ 3d gra jed/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float mouseSens;
+    public Transform playerBod;
+    public float XRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerBod = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+        XRotation -= mouseY;
+        XRotation = Mathf.Clamp(XRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
+        playerBod.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ 3d gra jed/Assets/Scripts/PlayerConroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConroller : MonoBehaviour
+{
+    public float speed = 12f;
+    public float grav = -10f;
+    Vector3 velocity;
+    CharacterController charController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        charController.Move(move * speed * Time.deltaTime);
+
+        velocity.y += grav * Time.deltaTime;
+        charController.Move(velocity * Time.deltaTime);
+    }
+}
