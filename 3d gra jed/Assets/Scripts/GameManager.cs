d18a12a650900caad5c5f71c4fffab2c1845c525
Kapo-Gorzki <79355604+Kapo-Gorzki@users.noreply.github.com>
2021-03-13 10:25:57 +0100

    PickUps + speed
    f

---
+++ 3d gra jed/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+    public int timeToEnd;
+    bool endGame = false;
+    bool gamePaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameManager = null)
+        {
+            gameManager = this;
+        }
+        if(timeToEnd <= 0)
+        {
+            timeToEnd = 30;
+        }
+        InvokeRepeating("Stopper", 2, 1);
+    }
+    public void EndGame()
+    {
+        CancelInvoke("Stopper");
+        Debug.Log("EndGame");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            if(gamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+         
+        }
+    }
+
+    void Stopper()
+    {
+        timeToEnd--;
+        Debug.Log("timeToEnd: " + timeToEnd + "s");
+        if(timeToEnd <= 0)
+        {
+            timeToEnd = 0;
+            endGame = true;
+        }
+        if(endGame)
+        {
+            EndGame();  
+        }
+    }
+
+    void PauseGame()
+    {
+        Debug.Log("Pause Game");
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    void ResumeGame()
+    {
+        Debug.Log("Resume Game");
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+}
--- 3d gra jed/Assets/Scripts/PlayerConroller.cs
+++ 3d gra jed/Assets/Scripts/PlayerConroller.cs
@@ -7,6 +7,9 @@ public class PlayerConroller : MonoBehaviour
     public float speed = 12f;
     public float grav = -10f;
     Vector3 velocity;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    bool isGrounded;
     CharacterController charController;
     // Start is called before the first frame update
     void Start()
@@ -21,8 +24,44 @@ public class PlayerConroller : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
         charController.Move(move * speed * Time.deltaTime);
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.4f, groundMask);
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
 
         velocity.y += grav * Time.deltaTime;
         charController.Move(velocity * Time.deltaTime);
+        RaycastHit hit;
+        if(Physics.Raycast(
+            groundCheck.position,
+            transform.TransformDirection(Vector3.down),
+            out hit,
+            0.4f,
+            groundMask))
+        {
+            string TerrainType;
+            TerrainType = hit.collider.gameObject.tag;
+            switch(TerrainType)
+            {
+                default:
+                    speed = 12;
+                    break;
+                case "LowPlane":
+                    speed = 3;
+                    break;
+                case "HigPlane":
+                    speed = 20;
+                    break;
+
+            }
+        }    
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.tag == "PickUp")
+        {
+            hit.gameObject.GetComponent<PickUps>().Picked();
+        }
     }
 }
