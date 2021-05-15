5873641eda653a0cc6a810ccf2dbaae90263d21e
Kapo-Gorzki <79355604+Kapo-Gorzki@users.noreply.github.com>
2021-05-15 10:35:24 +0200

    nazwa4

---
+++ 3d gra jed/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float HealthPoints = 100f;
+    public void TakeDamage()
+    {
+        HealthPoints -= damage;
+        if(HealthPoints <=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
